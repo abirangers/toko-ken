cda9812bff13555a5eb68d7b8c26ac46357602c7
abirangers <abiayyasi12@gmail.com>
2023-05-17 21:55:38 +0700

    merapikan code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,23 @@
 // OPEN & CLOSE CART
-const cartIcon = document.querySelector('#cart-icon');
-const cart = document.querySelector('.cart');
-const closeCart = document.querySelector('#cart-close');
-const btnNav = document.querySelector('.bars-right .bars');
-const eye = document.querySelectorAll('.eye');
-const detailProductParent = document.querySelector('.detail');
-const headerNav = document.querySelector('header');
+const cartIcon = document.querySelector("#cart-icon");
+const cart = document.querySelector(".cart");
+const closeCart = document.querySelector("#cart-close");
+const btnNav = document.querySelector(".bars-right .bars");
+const eye = document.querySelectorAll(".eye");
+const detailProductParent = document.querySelector(".detail");
+const headerNav = document.querySelector("header");
 
-
-btnNav.addEventListener('click', () => {
-    const ulNav = document.querySelector('.navbar');
-    ulNav.classList.toggle('aktif');
+btnNav.addEventListener("click", () => {
+  const ulNav = document.querySelector(".navbar");
+  ulNav.classList.toggle("aktif");
 });
 
-cartIcon.addEventListener('click', () => {
-    cart.classList.toggle('active');
+cartIcon.addEventListener("click", () => {
+  cart.classList.toggle("active");
 });
 
-closeCart.addEventListener('click', () => {
-    cart.classList.remove('active');
+closeCart.addEventListener("click", () => {
+  cart.classList.remove("active");
 });
 
 // eye.forEach(e => {
@@ -27,209 +26,211 @@ closeCart.addEventListener('click', () => {
 //     });
 // });
 
-
-
-
 // Start when the document is ready
-if(document.readyState == "loading") {
-    document.addEventListener('DOMContentLoaded', start);
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", start);
 } else {
-    start();
+  start();
 }
 
 //======================= START =======================
 function start() {
-    addEvents();
+  addEvents();
 }
-
-
-
-
 
 //======================= UPDATE & RERENDER =======================
 function update() {
-    addEvents();
-    updateTotal();
-    // handle_addDetailItem();
+  addEvents();
+  updateTotal();
+  // handle_addDetailItem();
 }
-
-
-
 
 //======================= UPDATE & RERENDER =======================
 function addEvents() {
-    // Remove items from cart
-    let cartRemove_btns = document.querySelectorAll('.cart-remove');
-    cartRemove_btns.forEach(btn => {
-        btn.addEventListener('click', handle_removeCartItem);
-    });
-    // Change item quantity
-    let cartQuantity_inputs = document.querySelectorAll('.cart-quantity');
-    cartQuantity_inputs.forEach(input => {
-        input.addEventListener('change', handle_changeItemQuantity);
-    });
+  // Remove items from cart
+  let cartRemove_btns = document.querySelectorAll(".cart-remove");
+  cartRemove_btns.forEach((btn) => {
+    btn.addEventListener("click", handle_removeCartItem);
+  });
+  // Change item quantity
+  let cartQuantity_inputs = document.querySelectorAll(".cart-quantity");
+  cartQuantity_inputs.forEach((input) => {
+    input.addEventListener("change", handle_changeItemQuantity);
+  });
 
-    // Add item to cart
-    let addCart_btns = document.querySelectorAll('.add-cart');
-    addCart_btns.forEach(btn => {
-        btn.addEventListener('click', handle_addCartItem);
-    });
+  // Add item to cart
+  let addCart_btns = document.querySelectorAll(".add-cart");
+  addCart_btns.forEach((btn) => {
+    btn.addEventListener("click", handle_addCartItem);
+  });
 
-    // add item to detail item
-    const eye = document.querySelectorAll('.eye');
-    eye.forEach(e => {
-        e.addEventListener('click', handle_addDetailItem);
-    });
+  // add item to detail item
+  const eye = document.querySelectorAll(".eye");
+  eye.forEach((e) => {
+    e.addEventListener("click", handle_addDetailItem);
+  });
 
-    
-
-    // Buy Order
-    const buy_btn = document.querySelector('.btn-buy');
-    buy_btn.addEventListener('click', handle_buyOrder);
+  // Buy Order
+  const buy_btn = document.querySelector(".btn-buy");
+  buy_btn.addEventListener("click", handle_buyOrder);
 }
 
 function getProductData(product) {
-    let title = product.querySelector('.product-title').innerHTML;
-    let price = product.querySelector('.product-price').innerHTML;
-    let imgSrc = product.querySelector('.product-img').src;
+  let title = product.querySelector(".product-title").innerHTML;
+  let price = product.querySelector(".product-price").innerHTML;
+  let imgSrc = product.querySelector(".product-img").src;
 
-    return {
-        title,
-        price,
-        imgSrc
-    };
+  return {
+    title,
+    price,
+    imgSrc,
+  };
 }
-
-
-
 
 let itemsAdded = [];
 //======================= HANDLE EVENTS FUNCTION =======================
 function handle_addCartItem() {
-    let product = this.parentElement.parentElement;
-    let productData = getProductData(product);
-    
+  let product = this.parentElement.parentElement;
+  let productData = getProductData(product);
 
-    let newToAdd = {
-        title: productData.title,
-        price: productData.price,
-        imgSrc: productData.imgSrc,
-    };
+  let newToAdd = {
+    title: productData.title,
+    price: productData.price,
+    imgSrc: productData.imgSrc,
+  };
 
-    // handle item is already exist
-    if(itemsAdded.find(el => el.title == newToAdd.title)){
-        alert("This Item is Already Exist!");
-        return;
-    } else {
-        itemsAdded.push(newToAdd);
-    }
-    
-    // Add product to cart
-    let cartBoxElement = CartBoxComponent(newToAdd.title, newToAdd.price, newToAdd.imgSrc);
-    let newNode = document.createElement("div");
-    newNode.innerHTML = cartBoxElement;
-    const cartContent = cart.querySelector(".cart-content");
-    cartContent.appendChild(newNode);
-    update();
+  // handle item is already exist
+  if (itemsAdded.find((el) => el.title == newToAdd.title)) {
+    alert("This Item is Already Exist!");
+    return;
+  } else {
+    itemsAdded.push(newToAdd);
+  }
+
+  // Add product to cart
+  let cartBoxElement = CartBoxComponent(
+    newToAdd.title,
+    newToAdd.price,
+    newToAdd.imgSrc
+  );
+  let newNode = document.createElement("div");
+  newNode.innerHTML = cartBoxElement;
+  const cartContent = cart.querySelector(".cart-content");
+  cartContent.appendChild(newNode);
+  update();
 }
 
 function handle_removeCartItem() {
-    this.parentElement.remove();
-    itemsAdded = itemsAdded.filter(el => el.title != this.parentElement.querySelector('.cart-product-title').innerHTML);
+  this.parentElement.remove();
+  itemsAdded = itemsAdded.filter(
+    (el) =>
+      el.title !=
+      this.parentElement.querySelector(".cart-product-title").innerHTML
+  );
 
-    update();
+  update();
 }
 
 function handle_changeItemQuantity() {
-    if(isNaN(this.value) || this.value < 1) {
-        this.value = 1;
-    }
-    this.value = Math.floor(this.value); // to keep it integer
+  if (isNaN(this.value) || this.value < 1) {
+    this.value = 1;
+  }
+  this.value = Math.floor(this.value); // to keep it integer
 
-    update();
+  update();
 }
 
-
 function handle_buyOrder() {
-    if(itemsAdded.length <= 0) {
-        alert('There is No Order to Place Yet! \n Please Make an Order first');
-        return;
-    }
-    const cartContent = cart.querySelector('.cart-content');
-    cartContent.innerHTML = '';
-    alert('Your Order is Placed Successfully :)');
-    itemsAdded = [];
+  if (itemsAdded.length <= 0) {
+    alert("There is No Order to Place Yet! \n Please Make an Order first");
+    return;
+  }
+  const cartContent = cart.querySelector(".cart-content");
+  cartContent.innerHTML = "";
+  alert("Your Order is Placed Successfully :)");
+  itemsAdded = [];
 
-    update();
+  update();
 }
 
 //======================= UPDATE & RERENDER FUNCTIONS =======================
 function updateTotal() {
-    let cartBoxes = document.querySelectorAll('.cart-box');
-    const totalElement = cart.querySelector('.total-price');
-    let total = 0;
-    cartBoxes.forEach(cartBox => {
-        let priceElement = cartBox.querySelector('.cart-price');
-        let price = priceElement.innerHTML.replace('Rp.', '').replace('.', '');
-        let quantity = cartBox.querySelector('.cart-quantity').value;
-        total += price * quantity;
-    });
-    total = Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', notation: 'compact' }).format(total);
-    totalElement.innerHTML = total;
+  let cartBoxes = document.querySelectorAll(".cart-box");
+  const totalElement = cart.querySelector(".total-price");
+  let total = 0;
+  cartBoxes.forEach((cartBox) => {
+    let priceElement = cartBox.querySelector(".cart-price");
+    let price = priceElement.innerHTML.replace("Rp.", "").replace(".", "");
+    let quantity = cartBox.querySelector(".cart-quantity").value;
+    total += price * quantity;
+  });
+  total = Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    notation: "compact",
+  }).format(total);
+  totalElement.innerHTML = total;
 }
 
 function handle_addDetailItem() {
-    detailProductParent.classList.add('fixed');
-    let product = this.parentElement.parentElement;
-    let productData = getProductData(product);
-    detailProductParent.innerHTML = detailProduct(productData.title, productData.price, productData.imgSrc);
+  detailProductParent.classList.add("fixed");
+  let product = this.parentElement.parentElement;
+  let productData = getProductData(product);
+  detailProductParent.innerHTML = detailProduct(
+    productData.title,
+    productData.price,
+    productData.imgSrc
+  );
 
-    const kembali = document.querySelectorAll('.kembali');
-    kembali.forEach(k => {
-        k.addEventListener('click', (e) => {
-            detailProductParent.classList.remove('fixed');
-            e.preventDefault();
-        });
+  const kembali = document.querySelectorAll(".kembali");
+  kembali.forEach((k) => {
+    k.addEventListener("click", (e) => {
+      detailProductParent.classList.remove("fixed");
+      e.preventDefault();
     });
-    
-    window.addEventListener('click', (e) => {
-        if(e.target === detailProductParent) {
-            detailProductParent.classList.remove('fixed');
-            detailProductParent.removeChild(newNodeDetail);
-        };
+  });
+
+  window.addEventListener("click", (e) => {
+    if (e.target === detailProductParent) {
+      detailProductParent.classList.remove("fixed");
+      detailProductParent.removeChild(newNodeDetail);
+    }
+  });
+  const buttonDetail = document.querySelectorAll(".button-detail");
+  buttonDetail.forEach((bd) => {
+    bd.addEventListener("click", function (e) {
+      e.preventDefault();
+      let newToAdd = {
+        title: productData.title,
+        price: productData.price,
+        imgSrc: productData.imgSrc,
+      };
+
+      // handle item is already exist
+      if (itemsAdded.find((el) => el.title == newToAdd.title)) {
+        alert("This Item is Already Exist!");
+        return;
+      } else {
+        itemsAdded.push(newToAdd);
+      }
+
+      let cartBoxElement = CartBoxComponent(
+        newToAdd.title,
+        newToAdd.price,
+        newToAdd.imgSrc
+      );
+      let newNode = document.createElement("div");
+      newNode.innerHTML = cartBoxElement;
+      const cartContent = cart.querySelector(".cart-content");
+      cartContent.appendChild(newNode);
+      update();
     });
-    const buttonDetail = document.querySelectorAll('.button-detail');
-    buttonDetail.forEach(bd => {
-        bd.addEventListener('click', function(e) {
-            e.preventDefault();
-            let newToAdd = {
-                title: productData.title,
-                price: productData.price,
-                imgSrc: productData.imgSrc,
-            };
-
-            // handle item is already exist
-            if(itemsAdded.find(el => el.title == newToAdd.title)){
-                alert("This Item is Already Exist!");
-                return;
-            } else {
-                itemsAdded.push(newToAdd);
-            }
-
-            let cartBoxElement = CartBoxComponent(newToAdd.title, newToAdd.price, newToAdd.imgSrc);
-            let newNode = document.createElement("div");
-            newNode.innerHTML = cartBoxElement;
-            const cartContent = cart.querySelector(".cart-content");
-            cartContent.appendChild(newNode);
-            update();
-
-        });
-    }); 
+  });
 }
 
 //======================= HTML COMPONENTS =======================
 function CartBoxComponent(title, price, imgSrc) {
-    return `
+  return `
     <div class="cart-box">
     <img src="${imgSrc}" alt="" class="cart-img">
     <div class="detail-box">
@@ -243,7 +244,7 @@ function CartBoxComponent(title, price, imgSrc) {
 }
 
 function detailProduct(title, price, imgSrc) {
-    return `
+  return `
         <div class="container-detail">
         <div class="container-image">
         <div class="content-image">
@@ -260,5 +261,5 @@ function detailProduct(title, price, imgSrc) {
                 <a href="#" class="kembali kembali-2">kembali</a>
                 </div>
             </div>
-        </div>`
+        </div>`;
 }
